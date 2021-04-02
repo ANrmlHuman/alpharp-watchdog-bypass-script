5b9f40c854c5c53e22c986ff15dd63973fdba48c
ANrmlHuman <80472603+ANrmlHuman@users.noreply.github.com>
2021-04-02 08:53:26 -0400

    Update alpharp_bypass.lua
    fixed to unpatched version

--- alpharp_bypass.lua
+++ alpharp_bypass.lua
@@ -1,5 +1,16 @@
-net.Start("wd_disableanticheat")
- net.WriteEntity(LocalPlayer())
- net.WriteBit(4)
+function GenKey()
+return string.char(math.random(97, 122))..math.random(0,9)..string.char(math.random(97, 122))..math.random(0,9).."-"..string.char(math.random(97, 122))..math.random(0,9)..string.char(math.random(97, 122))..math.random(0,9).."-"..string.char(math.random(97, 122))..math.random(0,9)..string.char(math.random(97, 122))..math.random(0,9)
+end	
+net.Start("AhgAjoadgk")
+--checks for aim target
+for k,v in pairs(ents.FindByClass("wd_ab_t") do
+if not LocalPlayer():Distance(v:GetPos()) < 25 then return end
+ net.WriteEntity(v)
+end
+ net.WriteBit(14)
+ --generates private staff key
+ net.WriteString(GenKey())
 net.SendToServer()
-print("made by github.com/ANrmlHuman")
+print("fixed by github.com/ANrmlHuman/")
+-- this was fixed from the patch
+ -- this was found by grabbing all of the nets on AlphaRP
