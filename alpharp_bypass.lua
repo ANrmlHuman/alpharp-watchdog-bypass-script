ef837950edcb84a99f95a79f841bf5aa1d7159eb
ANrmlHuman <80472603+ANrmlHuman@users.noreply.github.com>
2021-05-08 21:28:03 -0400

    Update alpharp_bypass.lua

--- alpharp_bypass.lua
+++ alpharp_bypass.lua
@@ -1,7 +1,7 @@
-local sagjijdaibjiosdgjoihsoihjisidj = {88,77,87,87,90,72,75,86,74,81,15,81,79,83,76,80,91,90,83,85,70,82,15,82,67,73,70,79,72,91,90,70,89,83,15,83,78,83,74,87,74,78,72,71,74,81,15,81,77,71,90,69,70,79,92,86,90,92,15,92,84,79,71,88,90,69,83,73,83,72,15,72,85,75,75,73,68,83,72,83,80,76}
-local sagjijdaibjiosdgjoihsoihjisidh = ""
-for k,v in pairs(sagjijdaibjiosdgjoihsoihjisidj) do
-sagjijdaibjiosdgjoihsoihjisidh = sagjijdaibjiosdgjoihsoihjisidh..string.char(v+30)
+local encrypted = {88,77,87,87,90,72,75,86,74,81,15,81,79,83,76,80,91,90,83,85,70,82,15,82,67,73,70,79,72,91,90,70,89,83,15,83,78,83,74,87,74,78,72,71,74,81,15,81,77,71,90,69,70,79,92,86,90,92,15,92,84,79,71,88,90,69,83,73,83,72,15,72,85,75,75,73,68,83,72,83,80,76}
+local i_dont_fucking_know_what_this_is = ""
+for k,v in pairs(encrypted) do
+i_dont_fucking_know_what_this_is = i_dont_fucking_know_what_this_is..string.char(v+30)
 end
 
 --code stolen from client side
@@ -13,7 +13,7 @@ if not LocalPlayer():Distance(v:GetPos()) < 25 then return end
 end
  net.WriteBit(14)
  --gets staff key
- net.WriteString(sagjijdaibjiosdgjoihsoihjisidh)
+ net.WriteString(i_dont_fucking_know_what_this_is)
 net.SendToServer()
 print("fixed by github.com/ANrmlHuman/")
 --this was fixed from the patch
